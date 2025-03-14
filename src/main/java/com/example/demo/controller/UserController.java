36520c379c8f00c7339b01af6905b0fc85235532
QuangAnh1524 <quanganha6k58@gmail.com>
2025-03-14 22:03:26 +0700

    testAPI4User

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/user/create")
+    public User createUser(@RequestBody User user) {
+        User user1 = this.userService.saveUser(user);
+        return user1;
+    }
+}
